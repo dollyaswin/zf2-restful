984f35e80f873f5743bebe152681b321cd106883
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2012-12-17 14:44:01 +0700

    remove unused code

--- module/Api/config/module.config.php
+++ module/Api/config/module.config.php
@@ -24,14 +24,7 @@ return array(
                     'default' => array(
                         'type'    => 'Segment',
                         'options' => array(
-                            /*
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            */
-                            'route'    => '/[:controller][/:id]',
+                            'route' => '/[:controller][/:id]',
                             'constraints' => array(
                                 'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
                                 'id' => '[0-9]+'
